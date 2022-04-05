bc00bc8ca2eda0f69d8badc144c225df0ead6a62
laaouatni <laaouatni.anas@outlook.it>
2022-04-05 19:58:49 +0200

    test

--- docs/src/javascript/script.js
+++ docs/src/javascript/script.js
@@ -6,6 +6,8 @@ let FORO_inputs = FORO_Form.querySelectorAll('input');
 let albero_inputs = albero_Form.querySelectorAll('input');
 
 
+let button
+
 /* get the data from the 2 forms and create a json object from the data */
 function getData() {
     let data = {};
@@ -28,25 +30,8 @@ function getData() {
 }
 
 function checkInputs() {
-    /* check if there are any inputs with blank or space character*/
-    let inputs = document.querySelectorAll('input[type="number]');
-    let isValid;
-
-    inputs.forEach((item, index) => {
-        if (item.value == '') {
-            item.border = '1px solid red';
-            alert.classList.add("alert-visible");
-            /* after 2 seconds remove the alert*/
-            setTimeout(() => {
-                alert.classList.remove("alert-visible");
-                item.border = 'inherit';
-            }, 2000);
-
-            return isValid = false;
-        }
-    });
-
-    return isValid = true;
+    FORO_Form.submit().preventDefault();
+    albero_Form.submit().preventDefault();
 }
 
 function showAlert() {
@@ -60,9 +45,6 @@ function showAlert() {
 
 function submitForm() {
     checkInputs();
-    console.log(checkInputs());
 
-    if (checkInputs()) {
-        getData();
-    }
+    getData();
 }
