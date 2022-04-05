feecca5755cfdacc44d4610d9f4bb8a91afd0f80
laaouatni <laaouatni.anas@outlook.it>
2022-04-05 21:47:13 +0200

    mini if else

--- docs/src/javascript/script.js
+++ docs/src/javascript/script.js
@@ -5,8 +5,7 @@ let albero_Form = document.getElementById('form-albero');
 let FORO_inputs = FORO_Form.querySelectorAll('input');
 let albero_inputs = albero_Form.querySelectorAll('input');
 
-
-let button
+let output = document.getElementById('output');
 
 /* get the data from the 2 forms and create a json object from the data */
 function getData() {
@@ -15,11 +14,20 @@ function getData() {
     let albero_data = {};
 
     FORO_inputs.forEach((item, index) => {
-        FORO_data[item.name] = item.value;
+        if (item.value.length != 0) {
+            FORO_data[item.name] = item.value.replace(",", ".");
+        } else {
+            showAlert();
+
+        }
     });
 
     albero_inputs.forEach((item, index) => {
-        albero_data[item.name] = item.value;
+        if (item.value.length != 0) {
+            albero_data[item.name] = item.value.replace(",", ".");
+        } else {
+            showAlert();
+        }
     });
 
     data['FORO'] = FORO_data;
@@ -29,22 +37,47 @@ function getData() {
     return data;
 }
 
-function checkInputs() {
-    FORO_Form.submit().preventDefault();
-    albero_Form.submit().preventDefault();
-}
-
 function showAlert() {
     alert.classList.add("alert-visible");
     /* after 2 seconds remove the alert*/
     setTimeout(() => {
         alert.classList.remove("alert-visible");
-        item.border = 'inherit';
     }, 2000);
 }
 
-function submitForm() {
-    checkInputs();
+function checkModalita() {
+    let data = getData();
 
-    getData();
+    let max_FORO = data['FORO']['max_FORO'];
+    let max_albero = data['albero']['max_albero'];
+
+    let min_FORO = data['FORO']['min_FORO'];
+    let min_albero = data['albero']['min_albero'];
+
+
+    /* libero */
+    if (min_FORO > max_albero) {
+        output.textContent = "libero";
+    }
+    /* interferenza */
+    else if (min_FORO < max_albero) {
+        output.textContent = "interferenza";
+        /* incerto */
+        if (min_FORO > min_albero) {
+            output.textContent = "incerto";
+        }
+    } else if (max_FORO < max_albero) {
+        output.textContent = "interferenza";
+    } else if (min_FORO > min_albero) {
+        output.textContent = "libero";
+        if (min_FORO > max_albero) {
+            output.textContent = "libero";
+        }
+    } else {
+        output.textContent = "questa non lo hai programmata";
+    }
+}
+
+function submitForm() {
+    checkModalita();
 }
