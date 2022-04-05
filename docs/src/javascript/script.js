ec4e8affbd059f9c2cf42b8c34e52138d90dc290
laaouatni <laaouatni.anas@outlook.it>
2022-04-05 18:31:42 +0200

    aggiunto buttoni e js selectors

---
+++ docs/src/javascript/script.js
@@ -0,0 +1,7 @@
+/* get the data from the 2 forms and create a json object from the data */
+
+let FORO_Form = document.getElementById('form-foro');
+let albero_Form = document.getElementById('albero-foro');
+
+let FORO_inputs = FORO_Form.querySelectorAll('input');
+let albero_inputs = albero_Form.querySelectorAll('input');
