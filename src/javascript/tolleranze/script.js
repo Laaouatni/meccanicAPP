c115f412b2653f30a4ab48683bfaf66761a7a282
laaouatni <laaouatni.anas@outlook.it>
2022-04-09 18:59:41 +0200

    solved bug 400 from api

--- src/javascript/tolleranze/script.js
+++ src/javascript/tolleranze/script.js
@@ -1,13 +1,27 @@
-let tolleranzeURL = "https://gsx2json.com/api?id=1LCd6P9pF9jYhiNQQn-Pu3Te-xlqkqwD78J4_ZuoOt34&sheet=h_tolleranze";
+let albero_excel_url = "https://gsx2json.com/api?id=1LCd6P9pF9jYhiNQQn-Pu3Te-xlqkqwD78J4_ZuoOt34";
 
-/* fetch data and transform it to json  */
-fetch(tolleranzeURL)
+let h_albero_num_URL = albero_excel_url + "&sheet=h_albero_numero";
+let h_albero_dati_URL = albero_excel_url + "&sheet=h_albero_dati";
+
+
+fetch(h_albero_num_URL)
     .then(function(response) {
         return response.json();
     })
     .then(function(data) {
         let tolleranze = data.columns;
 
-        document.getElementById("output-json").textContent = JSON.stringify(data);
+        document.getElementById("output-json").textContent += JSON.stringify(data);
+        console.log(tolleranze);
+    });
+
+fetch(h_albero_dati_URL)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        let tolleranze = data.columns;
+
+        document.getElementById("output-json").textContent += JSON.stringify(data);
         console.log(tolleranze);
     });
