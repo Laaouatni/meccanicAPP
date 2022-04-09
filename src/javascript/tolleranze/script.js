9c8f0d4627234fdc2fb01f0134a11afd7c6ba1f3
laaouatni <laaouatni.anas@outlook.it>
2022-04-09 18:42:43 +0200

    solved Mixed Content: The page at 'https://meccanicapp.vercel.app/src/pages/tolleranze.html' was loaded over HTTPS, but requested an insecure resource 'http://gsx2json.com/api?id=1LCd6P9pF9jYhiNQQn-Pu3Te-xlqkqwD78J4_ZuoOt34&sheet=h_tolleranze'. This request has been blocked; the content must be served over HTTPS. (anonymous) @ script.js:4 script.js:4          Uncaught (in promise) TypeError: Failed to fetch

--- src/javascript/tolleranze/script.js
+++ src/javascript/tolleranze/script.js
@@ -1,4 +1,4 @@
-let tolleranzeURL = "http://gsx2json.com/api?id=1LCd6P9pF9jYhiNQQn-Pu3Te-xlqkqwD78J4_ZuoOt34&sheet=h_tolleranze";
+let tolleranzeURL = "https://gsx2json.com/api?id=1LCd6P9pF9jYhiNQQn-Pu3Te-xlqkqwD78J4_ZuoOt34&sheet=h_tolleranze";
 
 /* fetch data and transform it to json  */
 fetch(tolleranzeURL)
