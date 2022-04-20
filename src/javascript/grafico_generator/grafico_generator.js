fb03ef00f96085c49a1ac44aa7695804390bbc62
laaouatni <laaouatni.anas@outlook.it>
2022-04-21 01:13:59 +0200

    delete file I dont need html

--- src/javascript/grafico_generator/grafico_generator.js
+++
@@ -1,6 +0,0 @@
-/* get the & in the url of the page */
-
-let url_param = window.location.search.substring(1);
-let url_param_array = url_param.split("&");
-
-let modalita = url_param_array[0].split("=")[1];
