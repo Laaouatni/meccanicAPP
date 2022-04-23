a33f8de7e68d50d1562926846404201b0a13f11f
laaouatni <laaouatni.anas@outlook.it>
2022-04-23 20:07:30 +0200

    beta test not completed

--- src/javascript/spianatura_gcode/spianatura_gcode.js
+++ src/javascript/spianatura_gcode/spianatura_gcode.js
@@ -14,36 +14,36 @@ let options = {
     "feed": "1000",
     "speed": "3000",
     "X0": pezzoGrezzo.X0,
-    "Y0": pezzoGrezzo.Y0
+    "Y0": pezzoGrezzo.Y0,
+    "nameGprogram": `%O0${10000 + Math.floor(Math.random() * 99999)}`
 };
+
+function init(options) {
+    G90orG91();
+    G54();
+
+    function addGnameProgram(options) {
+        if (options.nameGprogram != null) {}
+    }
+
+    function G90orG91(options) {
+        if (options.absolute) {
+            gcode.push(`G90`);
+        } else {
+            gcode.push(`G91`); // change to G91 then
+        }
+    }
+
+    function G54() {
+        gcode.push(`
+G54 `);
+    }
+}
 
 function createGcodeProgram(pezzoGrezzo, options) {
     let gcode = [];
 
     gcode.push("errore codice non giusto, sto ancora programmando l'app, non utilizzare!!! ")
-    if (options.absolute) {
-        gcode.push(`G90`);
-    } else {
-        gcode.push(`G90 test errore`); // change to G91 then
-    }
-
-    gcode.push(`G54`); // posizione zero pezzo
-    gcode.push(`M06 T${options.tool}`); // M06 T1
-    gcode.push(`G43 H${options.tool}`); // G43 H1000 Z3000
-
-    gcode.push(`G0 X0 Y0 Z100`); // prima di iniziare
-    gcode.push(`G0 Z${options.feed}`);
-    gcode.push(`G0 X${pezzoGrezzo.X0} Y${pezzoGrezzo.Y0}`);
-    gcode.push(`G0 Z0`);
-    gcode.push(`G0 X0 Y0`);
-    gcode.push(`G0 Z${options.feed}`);
-    gcode.push(`G0 X${pezzoGrezzo.X0} Y${pezzoGrezzo.Y0}`);
-    gcode.push(`G0 Z0`);
-    gcode.push(`G0 X0 Y0`);
-    gcode.push(`G0 Z${options.feed}`);
-    gcode.push(`G0 X${pezzoGrezzo.X0} Y${pezzoGrezzo.Y0}`);
-    gcode.push(`G0 Z0`);
-    gcode.push(`G0 X0 Y0`);
 
     // now create a var with the gcode program
     let gcodeProgram = gcode.join("<br>");
