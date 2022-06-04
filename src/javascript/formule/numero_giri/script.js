ca40a7750eb65cbd3bd9c9cbf6c07adfd6e5d384
laaouatni <laaouatni.anas@outlook.it>
2022-06-04 13:57:35 +0200

    now is working with <input type="number">

--- src/javascript/formule/numero_giri/script.js
+++ src/javascript/formule/numero_giri/script.js
@@ -2,11 +2,7 @@ let vtInput = document.querySelector('#vt-input');
 let diametroInput = document.querySelector('#diametro-input');
 let htmlOutput = document.querySelector('#html-output');
 
-let props = {
-    "vt": vtInput ? vtInput.value && console.log("✅") : "❌",
-    "diametro": diametroInput.value && console.log("✅") ? diametroInput.value : "❌",
-    "htmlOutput": htmlOutput,
-}
+
 
 function calcolaGiri(props) {
     console.log(props)
@@ -18,6 +14,12 @@ let arrayInput = [vtInput, diametroInput];
 
 arrayInput.forEach((input) => {
     input.addEventListener('input', () => {
+        let props = {
+            "vt": vtInput.value ? vtInput.value : "❌",
+            "diametro": diametroInput.value ? diametroInput.value : "❌",
+            "htmlOutput": htmlOutput,
+        }
+
         calcolaGiri(props);
     });
 });
