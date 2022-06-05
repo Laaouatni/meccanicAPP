f5eb89e6661e620c120b94f676680846b7febc3f
Laaouatni Anas <87947051+Laaouatni@users.noreply.github.com>
2022-06-05 21:44:23 +0200

    testing with console log the event listener

--- src/javascript/index/script.js
+++ src/javascript/index/script.js
@@ -3,6 +3,6 @@ let btn = document.querySelectorAll("a");
 btn.forEach((el) => {
     el.addEventListener("mouseover", () => {
         el.scrollIntoView({ behavior: "smooth", block: 'center' });
-        console.log("scrolled");
-    },  {once : true});
+        console.log("over");
+    },  );
 });
