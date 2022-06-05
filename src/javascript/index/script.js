7f5357c6d3f0bfa8e8d469edcb02d9a8d2877c6b
Laaouatni Anas <87947051+Laaouatni@users.noreply.github.com>
2022-06-05 21:48:54 +0200

    trying mouseenter event for solving the issue

--- src/javascript/index/script.js
+++ src/javascript/index/script.js
@@ -1,8 +1,8 @@
 let btn = document.querySelectorAll("a");
 
 btn.forEach((el) => {
-    el.addEventListener("mouseover", () => {
+    el.addEventListener("mouseenter", () => {
         el.scrollIntoView({ behavior: "smooth", block: 'center' });
-        console.log("over");
+        console.log("entered");
     },  );
 });
