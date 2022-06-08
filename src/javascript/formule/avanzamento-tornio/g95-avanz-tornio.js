740d71581b17542f11df7bdc3e2022216535cffb
laaouatni <laaouatni.anas@outlook.it>
2022-06-08 20:21:53 +0200

    finito la funzionalità G95 generatore avanzamento tornio

---
+++ src/javascript/formule/avanzamento-tornio/g95-avanz-tornio.js
@@ -0,0 +1,45 @@
+let raggioInput = document.querySelector("#raggio-utensile-input");
+let raggioSpan = document.querySelector("#raggio-utensile-span");
+
+let minHtml = document.querySelector('#html-outputMIN');
+let maxHtml = document.querySelector('#html-outputMAX');
+
+function calcolaRaggio(props) {
+    let formulaAvanzMax = (1 / 2) * props.raggio;
+    let formulaAvanzMin = (1 / 4) * props.raggio;
+
+    raggioSpan.textContent = `${props.raggio} mm raggio utensile`;
+
+    let results = {
+        raggio: props.raggio,
+        max: formulaAvanzMax,
+        min: formulaAvanzMin
+    }
+
+    props.maxHtml.textContent = results.max.toFixed(2);
+    props.minHtml.textContent = results.min.toFixed(2);
+
+    return results;
+}
+
+let props = {
+    "raggio": raggioInput.value / 10,
+    minHtml,
+    maxHtml
+}
+
+calcolaRaggio(props);
+
+let arrayInput = [raggioInput];
+
+arrayInput.forEach((input) => {
+    input.addEventListener('input', () => {
+        props = {
+            "raggio": raggioInput.value / 10,
+            minHtml,
+            maxHtml
+        }
+
+        calcolaRaggio(props);
+    }, { passive: true });
+});
