550ad8319d0655543bb7471ba8e6e546db2115d8
Laaouatni Anas <87947051+Laaouatni@users.noreply.github.com>
2022-06-09 10:14:05 +0200

    solved bug on scrolling, the scrollIntoView works before the <a> tag become bigger and take all the space. by adding mouseLeave, setTimeout, clearTimeout, window.innerWidth, mouseEnter

--- src/javascript/index/script.js
+++ src/javascript/index/script.js
@@ -2,7 +2,19 @@ let btn = document.querySelectorAll("a");
 
 btn.forEach((el) => {
     el.addEventListener("mouseenter", () => {
-        el.scrollIntoView({ behavior: "smooth", block: 'center' });
-        console.log("entered");
+        if (window.innerWidth < 800) {
+            el.scrollIntoView({ behavior: "smooth", block: 'center' });
+            console.log("entered on mobile");
+        } else {
+            let scrollAfterTime = setTimeout(() => {
+                el.scrollIntoView({ behavior: "smooth", block: 'center' });
+                console.log("pc, 1 second after, started scrolling");
+            }, 1500);
+
+            el.addEventListener('mouseleave', e => {
+                clearTimeout(scrollAfterTime);
+                console.log("leave on pc, before the 1500")
+            });
+        }
     });
 });
