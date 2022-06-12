84e1c01934588e0ea066d604e76474ae689a4025
laaouatni <laaouatni.anas@outlook.it>
2022-06-12 18:55:40 +0200

    delete service-worker because of lot of errors, maybe in the future I will retry

--- meccanicapp/sw.js
+++ meccanicapp/sw.js
@@ -1,3 +1,1 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.5.2/workbox-sw.js');
-
-workbox.routing.registerRoute(() => true, new workbox.strategies.NetworkFirst());
+/* console.log("in the future use workblock for PWA offline") */
