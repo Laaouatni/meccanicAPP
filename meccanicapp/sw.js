b410a35b911626c6ff1b8b165c15fe2e0cfd1839
Laaouatni Anas <87947051+Laaouatni@users.noreply.github.com>
2022-06-12 20:46:31 +0200

    testing new code from stackoverflow, if It will work in vercel production site, i will use it

--- meccanicapp/sw.js
+++ meccanicapp/sw.js
@@ -81,10 +81,21 @@ self.addEventListener("install", installEvent => {
     )
 });
 
-self.addEventListener("fetch", fetchEvent => {
-    fetchEvent.respondWith(
-        caches.match(fetchEvent.request).then(res => {
-            return res || fetch(fetchEvent.request)
-        })
-    )
+self.addEventListener('fetch', function(event) {
+  console.log('Fetch event for ', event.request.url);
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      if (response) {
+        console.log('Found ', event.request.url, ' in cache');
+        return response;
+      }
+      console.log('Network request for ', event.request.url);
+      return fetch(event.request)
+
+    }).catch(function(error) {
+
+      return caches.match('index.html');
+
+    })
+  );
 });
